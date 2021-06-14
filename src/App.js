2024bb2eab627dd8ff38dea067f102cce797fc70
chepre1986 <chepre1986@gmail.com>
2021-06-14 19:48:04 +0200

    link to my source

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
         <Search />
+        <br /><br /><br />
+        <small>
+        <a href="https://github.com/chepre1986/project_weather_app">Open-source</a> by Patrycja Szumska
+        </small>
       </div>
      );
 }
